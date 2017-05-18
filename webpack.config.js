312a9ee2d40b09a2c50d0ffb9fcf7b65ff962f65
Imanuel Gittens <gittensimanuel@gmail.com>
2017-05-17 21:02:22 -0400

    Complete grid. Add normalize and reset css. Update webpack to generate html file automatically.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 const SRC = path.join(__dirname, 'src/');
-const CSS = path.join(SRC, 'css/');
 const SASS = path.join(SRC, 'sass/');
 const NODE_MODULES = path.join(__dirname, 'node_modules/'); 
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+let ExtractTextPlugin = require("extract-text-webpack-plugin");
+let HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const extractSass = new ExtractTextPlugin({
     filename: "bundle.css"
@@ -16,7 +16,7 @@ module.exports = {
         filename: 'bundle.js'       // file name
     },
     resolve: {
-    	modules: [SRC, NODE_MODULES, CSS, SASS]
+    	modules: [SRC, NODE_MODULES, SASS]
     },
     module: {
         rules: [
@@ -40,7 +40,10 @@ module.exports = {
         ]
     },
     plugins: [
-	    extractSass
+	    extractSass,
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        })
 	  ],
     devServer: {
         contentBase: __dirname + '/dist',
